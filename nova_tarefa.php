08a9d0ef535de17aec8d40980b378445ce5663b8
robernar2011 <robernar2011@gmail.com>
2022-03-07 17:10:19 -0300

    Inserting records

--- nova_tarefa.php
+++ nova_tarefa.php
@@ -24,6 +24,12 @@
 			</div>
 		</nav>
 
+		<?PHP if ( isset($_GET['inclusao']) && $_GET['inclusao'] == 1) { ?>
+			<div class="bg-success pt-2 text-white d-flex justify-content-center">
+				<h5>Tarefa inserida com sucesso!</h5>
+			</div>
+		<?PHP } ?>
+
 		<div class="container app">
 			<div class="row">
 				<div class="col-md-3 menu">
