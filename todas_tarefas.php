d57feefe35f3a4bda63e1271ab6590fa1a8b4a2d
robernar2011 <robernar2011@gmail.com>
2022-03-08 18:10:00 -0300

    Mark task as done

--- todas_tarefas.php
+++ todas_tarefas.php
@@ -3,14 +3,18 @@
 	$acao = 'recuperar';
 	require 'tarefa_controller.php';
 
+	/*
+	echo '<pre>';
+	print_r($tarefas);
+	echo '</pre>';
+	*/
+
 ?>
 
-<!DOCTYPE html>
 <html>
 	<head>
-		<meta charset="utf-8">
+		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-
 		<title>App Lista Tarefas</title>
 
 		<link rel="stylesheet" href="css/estilo.css">
@@ -19,32 +23,33 @@
 
 		<script>
 			function editar(id, txt_tarefa) {
-				//Form de Edição
+
+				//criar um form de edição
 				let form = document.createElement('form')
 				form.action = 'tarefa_controller.php?acao=atualizar'
 				form.method = 'post'
 				form.className = 'row'
 
-				//Input para entrada do texto
+				//criar um input para entrada do texto
 				let inputTarefa = document.createElement('input')
 				inputTarefa.type = 'text'
 				inputTarefa.name = 'tarefa'
 				inputTarefa.className = 'col-9 form-control'
 				inputTarefa.value = txt_tarefa
 
-				//criar input hidden para guardar o id da tarefa
+				//criar um input hidden para guardar o id da tarefa
 				let inputId = document.createElement('input')
 				inputId.type = 'hidden'
 				inputId.name = 'id'
 				inputId.value = id
 
-				//Button para envio do form
+				//criar um button para envio do form
 				let button = document.createElement('button')
 				button.type = 'submit'
 				button.className = 'col-3 btn btn-info'
 				button.innerHTML = 'Atualizar'
 
-				//Incluir inputTarefa no form
+				//incluir inputTarefa no form
 				form.appendChild(inputTarefa)
 
 				//incluir inputId no form
@@ -53,26 +58,31 @@
 				//incluir button no form
 				form.appendChild(button)
 
+				//teste
+				//console.log(form)
+
 				//selecionar a div tarefa
-				let tarefa = document.getElementById('tarefa_' + id)
+				let tarefa = document.getElementById('tarefa_'+id)
 
 				//limpar o texto da tarefa para inclusão do form
 				tarefa.innerHTML = ''
 
 				//incluir form na página
 				tarefa.insertBefore(form, tarefa[0])
+
 			}
 
-			function remover(id){
+			function remover(id) {
 				location.href = 'todas_tarefas.php?acao=remover&id='+id;
 			}
 
+			function marcarRealizada(id) {
+				location.href = 'todas_tarefas.php?acao=marcarRealizada&id='+id;
+			}
 		</script>
-
 	</head>
 
 	<body>
-
 		<nav class="navbar navbar-light bg-light">
 			<div class="container">
 				<a class="navbar-brand" href="#">
@@ -86,9 +96,9 @@
 			<div class="row">
 				<div class="col-sm-3 menu">
 					<ul class="list-group">
-						<li class="list-group-item"><a href="index.php">Tarefas Pendentes</a></li>
-						<li class="list-group-item"><a href="nova_tarefa.php">Nova Tarefa</a></li>
-						<li class="list-group-item active"><a href="#">Todas Tarefas</a></li>
+						<li class="list-group-item"><a href="index.php">Tarefas pendentes</a></li>
+						<li class="list-group-item"><a href="nova_tarefa.php">Nova tarefa</a></li>
+						<li class="list-group-item active"><a href="#">Todas tarefas</a></li>
 					</ul>
 				</div>
 
@@ -96,24 +106,26 @@
 					<div class="container pagina">
 						<div class="row">
 							<div class="col">
-								<h4>Todas Tarefas</h4>
+								<h4>Todas tarefas</h4>
 								<hr />
 
-								<?PHP foreach ($tarefas as $indice => $tarefa) { ?>
-
+								<?php foreach($tarefas as $indice => $tarefa) { ?>
 									<div class="row mb-3 d-flex align-items-center tarefa">
-										<div class="col-sm-9" id="tarefa_<?= $tarefa->id ?>"> 
+										<div class="col-sm-9" id="tarefa_<?= $tarefa->id ?>">
 											<?= $tarefa->tarefa ?> (<?= $tarefa->status ?>)
 										</div>
 										<div class="col-sm-3 mt-2 d-flex justify-content-between">
 											<i class="fas fa-trash-alt fa-lg text-danger" onclick="remover(<?= $tarefa->id ?>)"></i>
-											<i class="fas fa-edit fa-lg text-info" onclick="editar(<?= $tarefa->id ?>, '<?= $tarefa->tarefa ?>')"></i>
-											<i class="fas fa-check-square fa-lg text-success"></i>
+											
+											<?php if($tarefa->status == 'pendente') { ?>
+												<i class="fas fa-edit fa-lg text-info" onclick="editar(<?= $tarefa->id ?>, '<?= $tarefa->tarefa ?>')"></i>
+												<i class="fas fa-check-square fa-lg text-success" onclick="marcarRealizada(<?= $tarefa->id ?>)"></i>
+											<?php } ?>
 										</div>
 									</div>
 
-								<?PHP } ?>
-
+								<?php } ?>
+								
 							</div>
 						</div>
 					</div>
